ad56b5fd75dff7837e1b799d5ba70a49da58396b
EvanRPavone <EvanRPavone@gmail.com>
2024-01-23 11:16:39 -0500

    added wind, wind gust and changed measurements to imperial

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -10,13 +10,6 @@ class ForecastsController < ApplicationController
       @weather = Rails.cache.fetch(@weather_cache_key, expires_in: 30.minutes) do
         WeatherService.call(@geocode.latitude, @geocode.longitude)          
       end
-      # CONVERT FROM CELSIUS TO FAHRENHEIT -> Round to nearest whole number
-      @temperature = (@weather.temperature * 9/5 + 32).round
-      @temperature_min = (@weather.temperature_min * 9/5 + 32).round
-      @temperature_max = (@weather.temperature_max * 9/5 + 32).round
-  
-      # CONVERT FROM MILLIBAR(mbar) TO INCH OF MERCURY(inHg) -> Round to 2 decimal places
-      @pressure = (@weather.pressure / 33.8639).round(2)
     rescue => e
       # If there are any errors from the Geocode service, return the error msg here
       flash.alert = e.message
--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -8,9 +8,11 @@ class WeatherService
     end    
     response = conn.get('/data/2.5/weather', {
       appid: Rails.application.credentials.openweather_api_key,
+      # passes in the lat and long retrieved from GeocodeService
       lat: latitude,
       lon: longitude,
-      units: "metric",
+      # sets unit of measurement to imperial
+      units: "imperial",
     })
     body = response.body
     body or raise IOError.new "OpenWeather response body failed"
@@ -25,10 +27,29 @@ class WeatherService
     weather.temperature = body["main"]["temp"]
     weather.temperature_min = body["main"]["temp_min"]
     weather.temperature_max = body["main"]["temp_max"]
+    weather.feels_like = body["main"]["feels_like"]
+    weather.wind = body["wind"]["speed"]
+    weather.wind_gust = body["wind"]["gust"]
     weather.humidity = body["main"]["humidity"]
     weather.pressure = body["main"]["pressure"]
     weather.description = body["weather"][0]["description"]
     weather
+    # ALL WEATHER DATA: 
+    # {
+    #   "coord"=>{"lon"=>-78.8498, "lat"=>35.8971}, 
+    #   "weather"=>[{"id"=>804, "main"=>"Clouds", "description"=>"overcast clouds", "icon"=>"04d"}],
+    #   "base"=>"stations", 
+    #   "main"=>{"temp"=>48.27, "feels_like"=>47.73, "temp_min"=>44.62, "temp_max"=>50.36, "pressure"=>1035, "humidity"=>47},
+    #   "visibility"=>10000, 
+    #   "wind"=>{"speed"=>3, "deg"=>311, "gust"=>4}, 
+    #   "clouds"=>{"all"=>100}, 
+    #   "dt"=>1706025683, 
+    #   "sys"=>{"type"=>2, "id"=>2031626, "country"=>"US", "sunrise"=>1706012517, "sunset"=>1706049118}, 
+    #   "timezone"=>-18000, 
+    #   "id"=>4480285, 
+    #   "name"=>"Morrisville", 
+    #   "cod"=>200
+    # }
   end
     
 end
