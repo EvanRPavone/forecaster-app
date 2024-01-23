7b17b9344eb1db98b744eed1b3f90df24ae37d9c
EvanRPavone <EvanRPavone@gmail.com>
2024-01-23 11:57:25 -0500

    update credentials 2

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,8 +1,8 @@
 Geocoder.configure(
   esri: {
     api_key: [
-      Rails.application.credentials.arcgis_api_user_id, 
-      Rails.application.credentials.arcgis_api_secret_key,
+      Rails.application.credentials.arcgis_user, 
+      Rails.application.credentials.arcgis_secret,
     ], 
     for_storage: true
   }
